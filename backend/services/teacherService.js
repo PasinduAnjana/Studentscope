ad33ebc298a335d47c5c9d92bf1198cb0c64dafb
PasinduAnjana <pasinduanjana000@gmail.com>
2025-08-07 00:19:50 +0530

    Update css

--- backend/services/teacherService.js
+++ backend/services/teacherService.js
@@ -4,8 +4,3 @@ exports.getStudents = async () => {
   const result = await pool.query("SELECT * FROM students");
   return result.rows;
 };
-
-exports.getStudentById = async (id) => {
-  const result = await pool.query("SELECT * FROM students WHERE id = $1", [id]);
-  return result.rows[0];
-};
